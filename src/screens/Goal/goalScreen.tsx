0229a770285ac2eaba1c38c371690dce0c316433
Kabaljit Singh <k.singh@fairfx.com>
2020-10-26 08:58:48 +0000

    feat(screen): Added content for goal

--- src/screens/Goal/goalScreen.tsx
+++ src/screens/Goal/goalScreen.tsx
@@ -17,7 +17,24 @@ export const GoalScreen: React.FunctionComponent<GoalScreenProps> = ({}) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Goal</Text>
+      <View style={styles.wrapper}>
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              "https://worldview.stratfor.com/sites/default/files/styles/article_full/public/cryptocurrency-gulf-display-shutterstock-1028639176.png?itok=K36RQG5A",
+          }}
+        ></Image>
+        <Text style={styles.text}>
+          In current time, everyday new digital assets are being develop and
+          adopted by the global community. The main assets we are known is
+          Bitcoin, which is trade over $4.1 billion every day. Why should not we
+          developer a platform to exchange these digital assets simpler and more
+          convenient for average human being. The main goal of this application
+          is to provide access to huge market for non-technical users. Where
+          they use this app to trade and own cryptocurrencies.
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -25,8 +42,24 @@ export const GoalScreen: React.FunctionComponent<GoalScreenProps> = ({}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-    paddingLeft: 8,
+    marginTop: StatusBar.currentHeight || 12,
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 18,
+    textAlign: "justify",
+    marginTop: 30,
+    lineHeight: 26,
+  },
+  wrapper: {
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 13,
   },
 });
 
--- src/screens/UserStories/userStoriesScreen.tsx
+++ src/screens/UserStories/userStoriesScreen.tsx
@@ -17,7 +17,32 @@ export const UserStoriesScreen: React.FunctionComponent<UserStoriesScreenProps> 
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>User Stories</Text>
+      <View style={styles.wrapper}>
+        <Image
+          style={styles.image}
+          source={{
+            uri:
+              "https://worldview.stratfor.com/sites/default/files/styles/article_full/public/cryptocurrency-gulf-display-shutterstock-1028639176.png?itok=K36RQG5A",
+          }}
+        ></Image>
+        <Text style={styles.text}>User 1: Buy currency</Text>
+        <Text style={styles.text}>
+          A 21 old year user want to buy a new cryptocurrency. But he doesn’t
+          have any knowledge about the currency. He will follow the next steps
+          to own the currency 1. From the home screen he will tap the search
+          icon to search the currency. 2. Once he found the currency, he will
+          tab on it 3. Then in a new screen he will be provide with more
+          information about the currency like price, description, volume and
+          other technical information. 4. If he is happy with the currency then
+          he will proceed to buy the currency, by tapping on buy button. 5. Then
+          he will be shown summary of his transaction with exchange rate and
+          quantity he wants to purchase. 6. Then we will be asked to enter his
+          bank details. 7. In the end we will be known success or failed screen
+          depending on status of the transaction 8. Once he is done with the
+          transaction, we will be redirected to currency screen which the bought
+          now with updated balance
+        </Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -25,8 +50,27 @@ export const UserStoriesScreen: React.FunctionComponent<UserStoriesScreenProps> 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
-    paddingLeft: 8,
+    marginTop: StatusBar.currentHeight || 12,
+    backgroundColor: "white",
+  },
+  text: {
+    fontSize: 18,
+    textAlign: "justify",
+    marginTop: 30,
+    lineHeight: 26,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  wrapper: {
+    paddingTop: 20,
+    paddingRight: 20,
+    paddingLeft: 20,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 13,
   },
 });
 
