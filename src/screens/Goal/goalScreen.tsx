7c35caba7bfc58e8ce687cb253228791d0241d26
Kabaljit Singh <k.singh@fairfx.com>
2020-10-30 16:12:33 +0000

    feat(screen): Added more copy in goal screen

--- src/screens/Goal/goalScreen.tsx
+++ src/screens/Goal/goalScreen.tsx
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Image,
 } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 import { GoalScreenProps } from "./goalScreen.models";
 
@@ -17,24 +18,43 @@ export const GoalScreen: React.FunctionComponent<GoalScreenProps> = ({}) => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.wrapper}>
-        <Image
-          style={styles.image}
-          source={{
-            uri:
-              "https://worldview.stratfor.com/sites/default/files/styles/article_full/public/cryptocurrency-gulf-display-shutterstock-1028639176.png?itok=K36RQG5A",
-          }}
-        ></Image>
-        <Text style={styles.text}>
-          In current time, everyday new digital assets are being develop and
-          adopted by the global community. The main assets we are known is
-          Bitcoin, which is trade over $4.1 billion every day. Why should not we
-          developer a platform to exchange these digital assets simpler and more
-          convenient for average human being. The main goal of this application
-          is to provide access to huge market for non-technical users. Where
-          they use this app to trade and own cryptocurrencies.
-        </Text>
-      </View>
+      <ScrollView>
+        <View style={styles.wrapper}>
+          <Image
+            style={styles.image}
+            source={{
+              uri:
+                "https://worldview.stratfor.com/sites/default/files/styles/article_full/public/cryptocurrency-gulf-display-shutterstock-1028639176.png?itok=K36RQG5A",
+            }}
+          ></Image>
+          <Text style={styles.text}>
+            In current time, everyday new digital assets are being develop and
+            adopted by the global community. The main asset we are known is
+            Bitcoin, which is trade over $4.1 billion every day. Why should not
+            we developer a platform to exchange these digital assets simpler and
+            more convenient for average human being. The main goal of this
+            application is to provide access to huge market for non-technical
+            users. Where they use this app to trade and own cryptocurrencies.
+          </Text>
+          <Text style={styles.text}>
+            The main goals of the app are the following:
+          </Text>
+          <Text style={styles.list}>* Ability to register and login</Text>
+          <Text style={styles.list}>
+            * View list of all currencies with price{" "}
+          </Text>
+          <Text style={styles.list}>
+            * Be able to buy a currency with a bank card
+          </Text>
+          <Text style={styles.list}>* View list of transctions</Text>
+          <Text style={styles.list}>* Overview of the investment</Text>
+
+          <Text style={styles.text}>
+            The main purpose of this app is to simplify purchase and managment
+            of crypto currencies.
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -50,6 +70,12 @@ const styles = StyleSheet.create({
     textAlign: "justify",
     marginTop: 30,
     lineHeight: 26,
+  },
+  list: {
+    fontSize: 18,
+    textAlign: "justify",
+    lineHeight: 26,
+    paddingLeft: 8,
   },
   wrapper: {
     paddingTop: 20,
