4c1b4b77d4e4212c81104594c9de88f0aabb927f
Kabaljit Singh <k.singh@fairfx.com>
2021-01-02 18:15:24 +0000

    feat(util): => Added color palette

---
+++ src/theme/colorPalette/index.ts
@@ -0,0 +1,50 @@
+
+  export const nestedColorPalette = {
+    mango: ['#FFB900', '#FFD566'],
+    midnightBlue: ['#262835'],
+    skyBlue: [
+      '#F7FAFD',
+      '#E5F2FC',
+      '#C0DEF8',
+      '#99CAF4',
+      '#75B6EF',
+      '#5DA6EC',
+      '#4C97E8',
+      '#4689DA',
+      '#3871C2',
+      '#3260B0',
+    ],
+    seaGreen: [
+      '#D5F1F3',
+      '#C1EAED',
+      '#ACE3E7',
+      '#9CDEE1',
+      '#82D5DA',
+      '#6DCDD3',
+      '#59C7CE',
+      '#2FB9C2',
+      '#238B92',
+      '#185D61',
+    ],
+    grey: [
+      '#F7F8FC',
+      '#EDEDF4',
+      '#E1E1EA',
+      '#D4D5DD',
+      '#BCBCC9',
+      '#9699AA',
+      '#7C7E8D',
+      '#676978',
+      '#484A58',
+    ],
+    red: ['#FFF2F1', '#D95852'],
+    yellow: ['#FFF8E4'],
+    green: ['#0BA866'],
+    white: ['#FFFFFF'],
+  };
+  
+  export const colorPalette = {
+    ...nestedColorPalette,
+  };
+
+  
