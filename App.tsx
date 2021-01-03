25aea53263a862007e145feeab6078aa86e4818e
Kabaljit Singh <k.singh@fairfx.com>
2021-01-03 15:49:40 +0000

    feat(config): => Added icon and splash screen

--- App.tsx
+++ App.tsx
@@ -1,15 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { ListSectionsScreen } from "./src/screens/ListSections";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { GoalScreen } from "./src/screens/Goal";
-import { UserStoriesScreen } from "./src/screens/UserStories/";
-import { MockScreen } from "./src/screens/Mock";
-import { ComparisonScreen } from "./src/screens/Comparison";
-import { StackScreen } from "./src/screens/Stack";
+import { HomeScreen } from "./src/screens/Home";
 
 const Stack = createStackNavigator();
 const backgroundColor = "#3871C2";
@@ -25,44 +20,9 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={ListSectionsScreen}
+          component={HomeScreen}
           options={{
             title: "Home",
-            ...options,
-          }}
-        />
-        <Stack.Screen
-          name="Goal"
-          component={GoalScreen}
-          options={{
-            ...options,
-          }}
-        />
-        <Stack.Screen
-          name="UserStories"
-          component={UserStoriesScreen}
-          options={{
-            ...options,
-          }}
-        />
-        <Stack.Screen
-          name="Mock"
-          component={MockScreen}
-          options={{
-            ...options,
-          }}
-        />
-        <Stack.Screen
-          name="Comparison"
-          component={ComparisonScreen}
-          options={{
-            ...options,
-          }}
-        />
-        <Stack.Screen
-          name="Stack"
-          component={StackScreen}
-          options={{
             ...options,
           }}
         />
