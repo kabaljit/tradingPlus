0a70cc05a67127bccbe5e174ee5408ebebcd0cd6
Kabaljit Singh <k.singh@fairfx.com>
2021-01-09 15:26:51 +0000

    feat(config): => Added firebase

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen } from "./src/screens/Home";
 import { LoginScreen } from "./src/screens/Login";
 import theme from "./src/theme";
+import * as firebase from "firebase";
 
 const Stack = createStackNavigator();
 const backgroundColor = theme.colorPalette.red[6];
@@ -17,6 +18,23 @@ const options = {
   headerTintColor: theme.colorPalette.white[0],
   cardStyle: { backgroundColor: theme.colorPalette.white[0] },
 };
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDcIRUEKSblScEI1JTL_YVQUr__h2-O0a0",
+  authDomain: "tradingplus-ee6fe.firebaseapp.com",
+  databaseURL:
+    "https://tradingplus-ee6fe-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "tradingplus-ee6fe",
+  storageBucket: "tradingplus-ee6fe.appspot.com",
+  messagingSenderId: "119488355341",
+  appId: "1:119488355341:web:9e1ec5ea0682434fc14944",
+  measurementId: "G-VQYHKB5TK3",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
 export default function App() {
   return (
     <NavigationContainer>
