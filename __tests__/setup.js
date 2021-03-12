a80a05fc34234da1dd2d9f43258ca5cd0d82b056
Kabaljit Singh <k.singh@fairfx.com>
2021-03-12 19:06:53 +0000

    feat(config): => Setup jest tests

---
+++ __tests__/setup.js
@@ -0,0 +1,8 @@
+// Any of code to setup the test
+
+
+console.log = jest.fn();
+console.warn = jest.fn();
+console.error = jest.fn();
+
+// jest.mock('reanimated-bottom-sheet');
--- src/screens/CoinDetail/coinDetailScreen.test.tsx
+++ src/screens/CoinDetail/coinDetailScreen.test.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
 
-import { CoinDetailScreen } from './coinDetailScreen';
-import { CoinDetailScreenProps } from './coinDetailScreen.models';
+import { CoinDetailScreen } from "./coinDetailScreen";
+import { CoinDetailScreenProps } from "./coinDetailScreen.models";
 
 const coinDetailScreenProps: CoinDetailScreenProps = {
+  ticker: "BTC",
 };
 
-describe('Given a CoinDetail screen, when it is rendered', () => {
-  it('should match the snapshot', () => {
-    const tree = renderer.create(<CoinDetailScreen {...coinDetailScreenProps} />).toJSON();
+describe("Given a CoinDetail screen, when it is rendered", () => {
+  it("should match the snapshot", () => {
+    const tree = renderer
+      .create(<CoinDetailScreen {...coinDetailScreenProps} />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
