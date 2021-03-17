a7148faa41cf368e4f20e0d9290269441e717da3
Kabaljit Singh <k.singh@fairfx.com>
2021-03-17 12:14:19 +0000

    feat(config): TR-4 Update the theme and using the theme on component

--- src/components/Box/box.tsx
+++ src/components/Box/box.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 
-import { useTheme } from '../../theme/provider/themeProvider';
-
 import { BoxProps } from './box.models';
 import { BoxView } from './box.styles';
 
@@ -9,10 +7,5 @@ export const Box: React.FunctionComponent<BoxProps> = ({
   children,
   ...propsRest
 }) => {
-  const theme = useTheme();
-  return (
-    <BoxView theme={theme} {...propsRest}>
-      {children}
-    </BoxView>
-  );
+  return <BoxView {...propsRest}>{children}</BoxView>;
 };
