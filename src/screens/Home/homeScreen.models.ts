3ec8ad73d76b2168ef4c8c8f82b9df9a2d6b17e3
Amaan Ansari <masteramaan0331@gmail.com>
2021-03-31 12:20:07 +0530

    feat(multi): TR-4 homeScreen and FLatlist Item

---
+++ src/screens/Home/homeScreen.models.ts
@@ -0,0 +1,10 @@
+export interface HomeScreenParams {
+  // Navigation parameters here.
+}
+
+export interface HomeScreenFormValues {
+  // Formik parameters here.
+}
+
+export interface HomeScreenProps extends HomeScreenParams {
+}
