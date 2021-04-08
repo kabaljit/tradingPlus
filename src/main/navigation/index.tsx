b91f459806c9401930a3f86b9b3183f2ad330113
Kabaljit Singh <k.singh@fairfx.com>
2021-04-08 23:00:35 +0100

    feat(main): TR-11 Corrected the app flow to match with right user experience

--- src/main/navigation/index.tsx
+++ src/main/navigation/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -7,6 +7,11 @@ import { LoginScreen } from '../../screens/Login';
 import { RegistrationScreen } from '../../screens/Registration';
 import theme from '../../theme';
 import { HomeScreen } from '../../screens/Home';
+import navigationService, {
+  isReadyRef,
+  navigationRef,
+} from '../../utils/navigationService';
+import firebase from '../../firebase';
 
 const Stack = createStackNavigator();
 
@@ -18,19 +23,45 @@ const options = {
   headerTintColor: theme.colors.tradingZ.white,
   cardStyle: { backgroundColor: theme.colors.tradingZ.white },
 };
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function Navigation() {
+  React.useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(
+          '[Navigation] failed to get prevent splash screen to hide: ',
+          e
+        );
+      }
+    }
+    prepare();
+  }, []);
+
+  React.useEffect(() => {
+    try {
+      // Getting user info
+      firebase.auth().onAuthStateChanged((user) => {
+        // Is user logged in ?
+        if (user) {
+          navigationService.navigate('Home');
+        } else {
+          navigationService.navigate('Login');
+        }
+        SplashScreen.hideAsync();
+      });
+    } catch (e) {
+      console.warn('[Navigation] failed to get user info: ', e);
+    }
+  }, []);
+
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator>
         <>
-          <Stack.Screen
-            name="Home"
-            component={HomeScreen}
-            options={{
-              ...options,
-            }}
-          />
           <Stack.Screen
             name="Login"
             component={LoginScreen}
@@ -45,6 +76,13 @@ export default function Navigation() {
             component={RegistrationScreen}
             options={{
               title: 'Signup',
+              ...options,
+            }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
               ...options,
             }}
           />
